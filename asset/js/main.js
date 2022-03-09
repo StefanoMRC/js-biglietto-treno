d7edeb32f73542ae276b39f3f2c9d0c0c312bd2f
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-09 15:08:12 +0100

    esercizio quasi finito

---
+++ asset/js/main.js
@@ -0,0 +1,30 @@
+let nomeUtente=prompt("Inserisci il tuo nome")
+document.getElementById("nome").innerHTML = nomeUtente;
+
+let cognomeUtente=prompt("Inserisci il tuo cognome")
+document.getElementById("cognome").innerHTML = cognomeUtente
+
+let kmDaPercorrere=prompt("Inserisci i chilometri da percorrere")
+document.getElementById("chilometri").innerHTML = kmDaPercorrere
+
+let eta=prompt("inserisci la tua eta")
+
+let prezzoBiglietto= kmDaPercorrere * 0.21;
+// document.getElementById("prezzo").innerHTML = prezzoBiglietto
+let scontoMinorenne= (prezzoBiglietto *20) / 100;
+let scontoAnziani= (prezzoBiglietto *40) / 100;
+if(eta >= 18 && eta <= 65) 
+{
+   document.getElementById("prezzo").innerHTML = prezzoBiglietto 
+   document.getElementById("sconto").innerHTML = "Intero"
+}
+else if(eta < 18)
+{
+    document.getElementById("prezzo").innerHTML = (prezzoBiglietto - scontoMinorenne) + " €";
+    document.getElementById("sconto").innerHTML = "Ridotto Minorenne"
+}
+else if(eta > 65)
+{
+    document.getElementById("prezzo").innerHTML = (prezzoBiglietto - scontoAnziani) + " €";
+    document.getElementById("sconto").innerHTML = "Ridotto Over 65"
+}
